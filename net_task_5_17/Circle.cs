b545dfadce2b9e854d4b963845eedbbf745f6230
ksmtrv <smotrova-kristina@mail.ru>
2023-04-25 23:45:34 +0300

    23:45

--- net_task_5_17/Circle.cs
+++ net_task_5_17/Circle.cs
@@ -4,6 +4,13 @@ namespace net_task_5_17
 {
     public class Circle : GeometricFigure
     {
+        public bool isFull { get; set; }
+
+        public Circle(bool isFull)
+        {
+            this.isFull = isFull;
+        }
+
         public double CountSectorArea(double angle = 10)
         {
             return Math.PI * length * length * angle / 360;
--- net_task_5_17/Triangle.cs
+++ net_task_5_17/Triangle.cs
@@ -4,8 +4,15 @@ namespace net_task_5_17
 {
     public class Triangle : GeometricFigure
     {
+        public bool isFull { get; set; }
         public double side { get; set; }
-        
+
+        public Triangle(bool isFull, double side)
+        {
+            this.isFull = isFull;
+            this.side = side;
+        }
+
         public double CountThirdSide()
         {
             return Math.Sqrt(length*length + side*side - 2*length*side*Math.Cos(angle*Math.PI*180));
